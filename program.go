41cd3f8e5e62a69554d042a23989b0eea7c52b09
Evgenii Mishalov <forwow495800@gmail.com>
2019-01-17 16:55:34 +0500

    initial commit

---
+++ program.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+func main() {
+	fmt.Printf("Server is running now!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		spew.Dump(r)
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":1488", nil)
+}
